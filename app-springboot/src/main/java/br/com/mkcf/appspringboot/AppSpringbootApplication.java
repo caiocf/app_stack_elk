a29ce8d6835193b051aefdeedd1ad2f8349bc4f5
Caio Ferreira <caio.c.f@hotmail.com>
2025-04-03 00:40:46 -0300

    feat: commit initial

---
+++ app-springboot/src/main/java/br/com/mkcf/appspringboot/AppSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.mkcf.appspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringbootApplication.class, args);
+	}
+
+}
---
+++ app-springboot/src/test/java/br/com/mkcf/appspringboot/AppSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mkcf.appspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
